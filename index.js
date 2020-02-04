c78acb325530cce388b3927cb98fd3cf70185a03
Jon Borgonia <jon.borgonia@gmail.com>
2020-02-03 20:06:48 -0800

    working traceroute with setTimeout

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const raw = require("raw-socket");
 
+const DESTINATION_IP = process.argv[2];
 const HEADER_LENGTH = 12;
-const DESTINATION_IP = "8.8.8.8";
-
-let header = Buffer.alloc(HEADER_LENGTH);
-header.writeUInt8(0x8, 0); //type
-header.writeUInt16LE(process.pid, 4); //id
-raw.writeChecksum(header, 2, raw.createChecksum(header));
+let found = false
 
 let socket = raw.createSocket({
   protocol: raw.Protocol.ICMP
@@ -23,20 +19,46 @@ socket.on("error", function(error) {
 });
 
 socket.on("message", function(buffer, source) {
-  console.log("received " + buffer.length + " bytes from " + source);
-  console.log("data: " + buffer.toString("hex"));
+  if(source === DESTINATION_IP){
+    found = true
+    const id = buffer.readUInt16BE(24)
+    console.log(`${id}\t${source}`)
+    console.log("destination reached at hop", id);
+    return
+  }
+  // console.log("received " + buffer.toString('hex'));
+  // const sourceIp = buffer.readUInt32BE(12)
+  const id = buffer.readUInt16BE(52)
+  console.log(`${id}\t${source}`)
+  // sourceIp = buffer.slice(12,16)
+  // console.log('sourceip', intToIpv4(sourceIp))
+  // console.timeEnd('hop:'+i)
 });
-
 var socketLevel = raw.SocketLevel.IPPROTO_IP;
 var socketOption = raw.SocketOption.IP_TTL;
 
-function beforeSend() {
-  socket.setOption(socketLevel, socketOption, 1);
+const sendICMP = (ttl) => {
+  let header = Buffer.alloc(HEADER_LENGTH);
+  header.writeUInt8(0x8, 0); //type
+  header.writeUInt16BE(ttl, 4); //id
+  raw.writeChecksum(header, 2, raw.createChecksum(header));
+
+  socket.send(header, 0, HEADER_LENGTH, DESTINATION_IP, function beforeSend() {
+    socket.setOption(socketLevel, socketOption, ttl);
+  }, function afterSend( error, bytes) {
+    if (error) console.log(error.toString());
+  });
 }
 
-socket.send(header, 0, HEADER_LENGTH, DESTINATION_IP, beforeSend, function(
-  error,
-  bytes
-) {
-  if (error) console.log(error.toString());
-});
+
+const hops = 64
+
+for(let i = 1; i < hops; i++){
+  console.time('hop:'+i)
+  setTimeout(() => {
+    if(!found){
+      sendICMP(i)
+    }
+  }, i*100)
+}
+
